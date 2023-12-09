cc6f1ba577b82c95e2d72f9f8773bb2cf88c5b4d
davi do carmo <davigcarmo02@gmail.com>
2023-12-08 21:55:08 -0300

    exercicios

---
+++ index.js
@@ -0,0 +1,29 @@
+salario1 = 100
+salario2 = 200
+salario3 = 300
+
+ir1 = 0
+ir2 = 10
+ir3 = 15
+  
+saliq1 = 100
+saliq2 = 190
+saliq3 = 285
+
+//expressao
+
+console.log(saliq1 >= salario1)
+
+console.log( salario2 < saliq2)
+
+
+
+
+
+A =2
+B = 3
+C = 5
+
+
+
+console.log((A+C) >B)
